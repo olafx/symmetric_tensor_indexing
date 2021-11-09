6d40603fb866ee4651a9f35e2267ce049e0d750e
Olaf Willocx <olaf@usa.com>
2021-11-09 20:13:44 +0100

    column major lower symmetric support

--- include/symmetric_tensor_indexing.cc
+++ include/symmetric_tensor_indexing.cc
@@ -1,16 +1,34 @@
 #include "symmetric_tensor_indexing.hh"
-
 #include <iostream>
 
 int main()
 {
     using namespace symmetric_tensor_indexing;
+    using std::cout;
 
-    size_t a = index(3, 3, 3);
-    // 19 = (3 + 4 - 1) choose (3) - 1
+    for (size_t i= 0; i < 4; i++)
+    {   cout << index_upper(i) << ' ';
+    }
+    cout << "\n\n";
 
-    size_t b = index(6, 6, 6, 6);
-    // 209 = (4 + 7 - 1) choose (4) - 1
+    for (size_t i= 0; i < 4; i++)
+    {   cout << index_lower(4, i) << ' ';
+    }
+    cout << "\n\n";
 
-    std::cout << a << ' ' << b << '\n';
+    for (size_t i= 0; i < 4; i++)
+    {   for (size_t j = 0; j < 4; j++)
+            cout << index_upper(i, j) << ' ';
+        cout << '\n';
+    }
+    cout << '\n';
+
+    for (size_t i= 0; i < 4; i++)
+    {   for (size_t j = 0; j < 4; j++)
+            cout << index_lower(4, i, j) << ' ';
+        cout << '\n';
+    }
+    cout << '\n';
+
+    cout << index_upper(6, 6, 6, 6) << ' ' << index_lower(7, 6, 6, 6, 6) << '\n';
 }
--- include/symmetric_tensor_indexing.hh
+++ include/symmetric_tensor_indexing.hh
@@ -5,11 +5,25 @@ namespace symmetric_tensor_indexing
 {
 
 template <size_t a, size_t b>
-size_t A(size_t i)
+size_t A_upper(size_t i)
 {   if constexpr (a == b)
-        return i;
+        return i + a;
+    else if constexpr (a == 0)
+        return i * A_upper<a + 1, b>(i);
     else
-        return A<a + 1, b>(i) * (i + a + 1);
+        return (i + a) * A_upper<a + 1, b>(i);
+}
+
+template <size_t a, size_t b>
+size_t A_lower(size_t n, size_t i)
+{   if constexpr (b == 0)
+        return i;
+    else if constexpr (a == b)
+        return a - 2 + 2 * n - i;
+    else if constexpr (a == 0)
+        return i * A_lower<a + 1, b>(n, i);
+    else
+        return (a - 2 + 2 * n - i) * A_lower<a + 1, b>(n, i);
 }
 
 template <size_t b>
@@ -20,22 +34,30 @@ size_t B(size_t i)
         return B<b - 1>(i / (b + 1));
 }
 
-template <size_t a>
-size_t C(size_t i)
-{   return B<a>(A<0, a>(i));
+template <size_t a, typename I, typename ...J>
+size_t C_upper(I i, J... j)
+{   if constexpr (sizeof...(j) == 0)
+        return B<a>(A_upper<0, a>(i));
+    else
+        return B<a>(A_upper<0, a>(i)) + C_upper<a + 1>(j...);
 }
 
 template <size_t a, typename I, typename ...J>
-size_t D(I i, J... j)
+size_t C_lower(size_t n, I i, J... j)
 {   if constexpr (sizeof...(j) == 0)
-        return C<a>(i);
+        return B<a>(A_lower<0, a>(n, i));
     else
-        return C<a>(i) + D<a + 1>(j...);
+        return B<a>(A_lower<0, a>(n, i)) + C_lower<a + 1>(n, j...);
 }
 
-template <typename ...I>
-size_t index(I... i)
-{   return D<0>(i...);
+template <typename I, typename ...J>
+size_t index_upper(I i, J... j)
+{   return C_upper<0>(i, j...);
+}
+
+template <typename I, typename ...J>
+size_t index_lower(size_t n, I i, J... j)
+{   return C_lower<0>(n, i, j...);
 }
 
 }
